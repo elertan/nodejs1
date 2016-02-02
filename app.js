9be3ca8e0d3dbb088d85cd705609b02d10c93ba0
Elertan <elertan@Elertans-MacBook-Pro.local>
2016-02-02 13:51:23 +0100

    Added bunch of dependencies and setup

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 // Serve static files
 app.use(express.static('bower_components'));
+app.use(express.static('public'));
 
 app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'jade');
@@ -15,6 +18,10 @@ app.get('/', function (req, res) {
 	res.render('index');
 });
 
-app.listen(port, function () {
+io.on('connection', function (socket) {
+	console.log('New connection');
+});
+
+http.listen(port, function () {
 	console.log('Server running on port ' + port);
 });
