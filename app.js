9414dbb0557848a52de7b0250a534ad4b490e4c1
Elertan <elertan@Elertans-MacBook-Pro.local>
2016-02-02 13:14:52 +0100

    Created starting files

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.get('/', function (req, res) {
+	res.end('Hello world!');
+});
+
+app.listen(port, function () {
+	console.log('Server running on port ' + port);
+});
